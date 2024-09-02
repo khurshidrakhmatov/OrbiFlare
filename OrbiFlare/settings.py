3d4f639bd01107ce9c73518c3b4b8f9d1ec72d40
Khurshid Rakhmatov <khurshidrakhmatov2002@gmail.com>
2024-09-02 23:08:16 +0500

    security
    sensitive files are hidden

--- OrbiFlare/settings.py
+++ OrbiFlare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#@nc&5-q!8nbbl)vu0%(d_$+g10=2(@lc!*kl=sr2bg63lnrw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -81,6 +82,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DJANGO_DB_NAME'),
+#         'USER': config('DJANGO_DB_USER'),
+#         'PASSWORD': config('DJANGO_DB_PASSWORD'),
+#         'HOST': config('DJANGO_DB_HOST'),
+#         'PORT': config('DJANGO_DB_PORT'),
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -114,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
